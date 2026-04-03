e43ef93c801a78085a4b318c8e02a31226bf9fd4
Raoulnjob <93221599+Raoulnjob@users.noreply.github.com>
2026-04-02 23:09:09 -0500

    Create WN10-CC-000327.ps1

---
+++ STIGS/WN10-CC-000327.ps1
@@ -0,0 +1,43 @@
+ <#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 1.
+
+.NOTES
+    Author          : Raoul NJOBI
+    LinkedIn        : https://www.linkedin.com/in/raoul-njobi-6a97333ab/
+    GitHub          : https://github.com/Raoulnjob/LognCyberSecurity/new/main
+    Date Created    : 2026-01-04
+    Last Modified   : 2026-01-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000327 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-CC-000327).ps1 
+#>
+# Run PowerShell as Administrator
+
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription"
+$ValueName = "EnableTranscripting"
+$ValueData = 1
+
+# Ensure the registry path exists
+if (-not (Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force | Out-Null
+}
+
+# Create or update the registry value (REG_DWORD)
+New-ItemProperty -Path $RegPath `
+    -Name $ValueName `
+    -Value $ValueData `
+    -PropertyType DWord `
+    -Force | Out-Null 
